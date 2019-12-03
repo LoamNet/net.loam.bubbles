3ea9fa7a8ebf7ae7b21d4a95ff0395746aeedcd4
Loam <loam.app@gmail.com>
2019-12-03 00:50:34 -0800

    + Added donation button

---
+++ Line Zen/Assets/Scripts/UI/UIOpenURL.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class UIOpenURL : MonoBehaviour
+{
+    public string targetURL;
+    private Button button;
+    void Start()
+    {
+        button = this.GetComponent<Button>();
+        button.onClick.AddListener(() => { Application.OpenURL(targetURL); });
+    }
+}
