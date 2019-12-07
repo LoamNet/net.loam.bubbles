107882dd0987e2444768bd70f8a96b8140359a56
Loam <loam.app@gmail.com>
2019-12-06 17:02:21 -0800

    ! going to 2020 now I guess?

--- Line Zen/Assets/Scripts/Game/GameEditorUtility.cs
+++ Line Zen/Assets/Scripts/Game/GameEditorUtility.cs
@@ -2,43 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-#if UNITY_EDITOR
-
-using System;
-using System.IO;
-using UnityEditor;
-
-[CustomEditor(typeof(GameEditorUtility))]
-public class GameEditorUtilityEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        GameEditorUtility util = (GameEditorUtility)this.target;
-        if (GUILayout.Button("Save As"))
-        {
-            string path = EditorUtility.SaveFilePanel(
-                "Save level", 
-                Environment.GetFolderPath(Environment.SpecialFolder.Desktop), 
-                "challenge0000", 
-                "txt");
-
-            if (!string.IsNullOrEmpty(path))
-            {
-                File.WriteAllText(path, util.Serialize());
-            }
-        }
-
-        if (GUILayout.Button("Clear All"))
-        {
-            util.Clear();
-        }
-
-        DrawDefaultInspector();
-    }
-}
-
-#endif
-
 // Custom editor for levels to make creating them easier
 public class GameEditorUtility : MonoBehaviour
 {
