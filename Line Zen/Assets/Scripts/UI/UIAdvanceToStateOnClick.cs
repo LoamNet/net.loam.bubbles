f156642fd4c18e4cd660bb4ace7ce0475d7bbaf6
Loam <loam.app@gmail.com>
2019-12-14 00:17:50 -0800

    = Updated outlines for challenges

--- Line Zen/Assets/Scripts/UI/UIAdvanceToStateOnClick.cs
+++ Line Zen/Assets/Scripts/UI/UIAdvanceToStateOnClick.cs
@@ -8,11 +8,30 @@ public class UIAdvanceToStateOnClick : MonoBehaviour
     public Events events;
     public GameState state;
     public GameMode mode;
+    public List<KeyCode> triggerKeys;
 
     void Start()
     {
         GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => {
             events.OnGameStateChangeRequest?.Invoke(state, mode);
         });
+    }
+
+    private void Update()
+    {
+        if(this.gameObject.activeInHierarchy)
+        {
+            if (triggerKeys != null)
+            {
+                foreach (KeyCode key in triggerKeys)
+                {
+                    if(Input.GetKeyDown(key))
+                    {
+                        events.OnGameStateChangeRequest?.Invoke(state, mode);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
