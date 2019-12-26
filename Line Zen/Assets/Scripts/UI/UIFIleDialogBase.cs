b6dec646ed19225f23583c0cf529a442b995367a
Loam <loam.app@gmail.com>
2019-12-25 17:47:55 -0800

    = Save dialog updates

--- Line Zen/Assets/Scripts/UI/UIFIleDialogBase.cs
+++ Line Zen/Assets/Scripts/UI/UIFIleDialogBase.cs
@@ -9,10 +9,52 @@ public class UIFIleDialogBase : MonoBehaviour
     public Button cancel;
     public Button select;
     public TMPro.TextMeshProUGUI textPath;
+    public GameObject entryTemplate;
+
+    private List<GameObject> entries;
+    private Transform entryParent;
+    private string _currentPath;
+    public string CurrentPath
+    {
+        get
+        {
+            return _currentPath;
+        }
+        set
+        {
+            _currentPath = value;
+            UpdateDialogWithDirectoryInfo();
+        }
+    }
+
+    // Collect some basic info and hide the template in the dialog.
+    void Start()
+    {
+        entryTemplate.SetActive(false);
+        entryParent = entryTemplate.gameObject.transform.parent;
+        CurrentPath = Application.dataPath;
+    }
 
     // Update is called once per frame
-    void Update()
+    void UpdateDialogWithDirectoryInfo()
     {
-            
+        textPath.text = _currentPath;
+        DirectoryInfo d = new DirectoryInfo(_currentPath);
+        FileInfo[] files = d.GetFiles();
+        
+        foreach(GameObject g in entries)
+        {
+            Destroy(g);
+        }
+        entries.Clear();
+
+        foreach (FileInfo file in files)
+        {
+            GameObject newLine = GameObject.Instantiate(entryTemplate, entryParent);
+            newLine.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = file.Name;
+            newLine.SetActive(true);
+
+            entries.Add(newLine);
+        }
     }
 }
