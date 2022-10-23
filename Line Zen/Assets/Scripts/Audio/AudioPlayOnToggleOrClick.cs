b123808406c8535471f2d3969b2f6a3c671f3a4e
Matthew Cech <contact@matthewcech.com>
2022-10-22 19:44:14 -0700

    Removing debug logging text

--- Line Zen/Assets/Scripts/Audio/AudioPlayOnToggleOrClick.cs
+++ Line Zen/Assets/Scripts/Audio/AudioPlayOnToggleOrClick.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class AudioPlayOnToggleOrClick : MonoBehaviour
 {
-    private void OnMouseEnter()
-    {
-        Debug.Log("KDSHFJLKJDSf");
-    }
     public void AudioOnPlayOnToggleOrClick()
     {
         GameAudio.Instance.PlaySoundEffect(GameAudio.Instance.ButtonOrToggleClick);
